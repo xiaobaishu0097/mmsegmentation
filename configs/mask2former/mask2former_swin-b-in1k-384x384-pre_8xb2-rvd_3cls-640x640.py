334cb209c8f5a028b9926bed68db545ddbbc0899
xiaobaishu0097 <du_heming@hotmail.com>
2023-06-05 00:15:18 +1000

    feat: :sparkles: include rvd related mask2former configs

--- configs/mask2former/mask2former_swin-b-in1k-384x384-pre_8xb2-rvd_3cls-640x640.py
+++ configs/mask2former/mask2former_swin-b-in1k-384x384-pre_8xb2-rvd_3cls-640x640.py
@@ -1,8 +1,10 @@
-_base_ = ['../_base_/default_runtime.py', '../_base_/datasets/rvd_2cls.py']
+_base_ = [
+    '../_base_/default_runtime.py', '../_base_/datasets/rvd_3cls_640x640.py'
+]
 
-custom_imports = dict(imports='mmdet.models', allow_failed_imports=False)
+pretrained = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/swin/swin_base_patch4_window12_384_20220317-55b0104a.pth'  # noqa
 
-crop_size = (512, 512)
+crop_size = (640, 640)
 data_preprocessor = dict(
     type='SegDataPreProcessor',
     mean=[123.675, 116.28, 103.53],
@@ -10,25 +12,34 @@ data_preprocessor = dict(
     bgr_to_rgb=True,
     pad_val=0,
     seg_pad_val=255,
-    size=crop_size,
-    test_cfg=dict(size_divisor=32))
-num_classes = 2
+    size=crop_size)
+num_classes = 3
+
+depths = [2, 2, 18, 2]
 model = dict(
     type='EncoderDecoder',
     data_preprocessor=data_preprocessor,
     backbone=dict(
-        type='ResNet',
-        depth=50,
-        deep_stem=False,
-        num_stages=4,
+        type='SwinTransformer',
+        pretrain_img_size=384,
+        embed_dims=128,
+        depths=depths,
+        num_heads=[4, 8, 16, 32],
+        window_size=12,
+        mlp_ratio=4,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.,
+        attn_drop_rate=0.,
+        drop_path_rate=0.3,
+        patch_norm=True,
         out_indices=(0, 1, 2, 3),
+        with_cp=False,
         frozen_stages=-1,
-        norm_cfg=dict(type='SyncBN', requires_grad=False),
-        style='pytorch',
-        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
+        init_cfg=dict(type='Pretrained', checkpoint=pretrained)),
     decode_head=dict(
         type='Mask2FormerHead',
-        in_channels=[256, 512, 1024, 2048],
+        in_channels=[128, 256, 512, 1024],
         strides=[4, 8, 16, 32],
         feat_channels=256,
         out_channels=256,
@@ -141,33 +152,50 @@ train_pipeline = [
     dict(type='LoadAnnotations', reduce_zero_label=False),
     dict(
         type='RandomChoiceResize',
-        scales=[int(512 * x * 0.1) for x in range(5, 21)],
+        scales=[int(x * 0.1 * 640) for x in range(5, 21)],
         resize_type='ResizeShortestEdge',
-        max_size=2048),
-    # dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
-    dict(type='Resize', scale=(1024, 1024), keep_ratio=True),
+        max_size=2560),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', prob=0.5),
     dict(type='PhotoMetricDistortion'),
     dict(type='PackSegInputs')
 ]
 train_dataloader = dict(batch_size=2, dataset=dict(pipeline=train_pipeline))
 
-# optimizer
+# set all layers in backbone to lr_mult=0.1
+# set all norm layers, position_embeding,
+# query_embeding, level_embeding to decay_multi=0.0
+backbone_norm_multi = dict(lr_mult=0.1, decay_mult=0.0)
+backbone_embed_multi = dict(lr_mult=0.1, decay_mult=0.0)
 embed_multi = dict(lr_mult=1.0, decay_mult=0.0)
+custom_keys = {
+    'backbone': dict(lr_mult=0.1, decay_mult=1.0),
+    'backbone.patch_embed.norm': backbone_norm_multi,
+    'backbone.norm': backbone_norm_multi,
+    'absolute_pos_embed': backbone_embed_multi,
+    'relative_position_bias_table': backbone_embed_multi,
+    'query_embed': embed_multi,
+    'query_feat': embed_multi,
+    'level_embed': embed_multi
+}
+custom_keys.update({
+    f'backbone.stages.{stage_id}.blocks.{block_id}.norm': backbone_norm_multi
+    for stage_id, num_blocks in enumerate(depths)
+    for block_id in range(num_blocks)
+})
+custom_keys.update({
+    f'backbone.stages.{stage_id}.downsample.norm': backbone_norm_multi
+    for stage_id in range(len(depths) - 1)
+})
+# optimizer
 optimizer = dict(
     type='AdamW', lr=0.0001, weight_decay=0.05, eps=1e-8, betas=(0.9, 0.999))
 optim_wrapper = dict(
     type='OptimWrapper',
     optimizer=optimizer,
     clip_grad=dict(max_norm=0.01, norm_type=2),
-    paramwise_cfg=dict(
-        custom_keys={
-            'backbone': dict(lr_mult=0.1, decay_mult=1.0),
-            'query_embed': embed_multi,
-            'query_feat': embed_multi,
-            'level_embed': embed_multi,
-        },
-        norm_decay_mult=0.0))
+    paramwise_cfg=dict(custom_keys=custom_keys, norm_decay_mult=0.0))
+
 # learning policy
 param_scheduler = [
     dict(
@@ -181,7 +209,7 @@ param_scheduler = [
 
 # training schedule for 160k
 train_cfg = dict(
-    type='IterBasedTrainLoop', max_iters=20000, val_interval=1000)
+    type='IterBasedTrainLoop', max_iters=160000, val_interval=5000)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 default_hooks = dict(
--- configs/mask2former/mask2former_swin-s_8xb2-rvd_2cls-512x512.py
+++ configs/mask2former/mask2former_swin-s_8xb2-rvd_2cls-512x512.py
@@ -1,26 +1,11 @@
-_base_ = ['./mask2former_r50_8xb2-160k_ade20k-512x512.py']
-pretrained = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/swin/swin_tiny_patch4_window7_224_20220317-1cdeb081.pth'  # noqa
-depths = [2, 2, 6, 2]
+_base_ = ['./mask2former_swin-t_8xb2-rvd_2cls-512x512.py']
+pretrained = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/swin/swin_small_patch4_window7_224_20220317-7ba6d6dd.pth'  # noqa
+
+depths = [2, 2, 18, 2]
 model = dict(
     backbone=dict(
-        _delete_=True,
-        type='SwinTransformer',
-        embed_dims=96,
-        depths=depths,
-        num_heads=[3, 6, 12, 24],
-        window_size=7,
-        mlp_ratio=4,
-        qkv_bias=True,
-        qk_scale=None,
-        drop_rate=0.,
-        attn_drop_rate=0.,
-        drop_path_rate=0.3,
-        patch_norm=True,
-        out_indices=(0, 1, 2, 3),
-        with_cp=False,
-        frozen_stages=-1,
-        init_cfg=dict(type='Pretrained', checkpoint=pretrained)),
-    decode_head=dict(in_channels=[96, 192, 384, 768]))
+        depths=depths, init_cfg=dict(type='Pretrained',
+                                     checkpoint=pretrained)))
 
 # set all layers in backbone to lr_mult=0.1
 # set all norm layers, position_embeding,
