4b9dbdafe52e5af1134f88ce2df8269a7085438e
xiaobaishu0097 <du_heming@hotmail.com>
2023-06-05 01:34:50 +1000

    feat: :sparkles: include rvd related unet configs

---
+++ configs/unet/unet-s5-d16_deeplabv3_4xb4-rvd_9cls-256x256.py
@@ -0,0 +1,13 @@
+_base_ = [
+    '../_base_/models/deeplabv3_unet_s5-d16.py', '../_base_/datasets/rvd_9cls_256x256.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
+]
+crop_size = (256, 256)
+data_preprocessor = dict(size=crop_size)
+model = dict(
+    data_preprocessor=data_preprocessor,
+    decode_head=dict(
+        num_classes=9,),
+    auxiliary_head=dict(
+        num_classes=9,),
+    test_cfg=dict(crop_size=(256, 256), stride=(170, 170)))
---
+++ configs/unet/unet-s5-d16_pspnet_4xb4-rvd_3cls-256x256.py
@@ -0,0 +1,13 @@
+_base_ = [
+    '../_base_/models/pspnet_unet_s5-d16.py', '../_base_/datasets/rvd_3cls_256x256.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
+]
+crop_size = (256, 256)
+data_preprocessor = dict(size=crop_size)
+model = dict(
+    data_preprocessor=data_preprocessor,
+    decode_head=dict(
+        num_classes=3,),
+    auxiliary_head=dict(
+        num_classes=3,),
+    test_cfg=dict(crop_size=(256, 256), stride=(170, 170)))
