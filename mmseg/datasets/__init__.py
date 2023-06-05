267ce4b03e1f435fbde66abfda3ba2ec402979e0
xiaobaishu0097 <du_heming@hotmail.com>
2023-06-06 01:39:14 +1000

    feat: :sparkles: add codes for RITE dataset

--- mmseg/datasets/__init__.py
+++ mmseg/datasets/__init__.py
@@ -33,9 +33,12 @@ from .transforms import (CLAHE, AdjustGamma, BioMedical3DPad,
                          RandomRotFlip, Rerange, ResizeShortestEdge,
                          ResizeToMultiple, RGB2Gray, SegRescale)
 from .voc import PascalVOCDataset
+
 from .rvd_2cls import RVD_2cls
 from .rvd_3cls import RVD_3cls
 from .rvd_9cls import RVD_9cls
+
+from .rite import RITE
 
 # yapf: enable
 __all__ = [
@@ -54,5 +57,7 @@ __all__ = [
     'BioMedicalGaussianNoise', 'BioMedicalGaussianBlur',
     'BioMedicalRandomGamma', 'BioMedical3DPad', 'RandomRotFlip',
     'SynapseDataset', 'REFUGEDataset', 'MapillaryDataset_v1',
-    'MapillaryDataset_v2', 'RVD_2cls', 'RVD_3cls', 'RVD_9cls',
+    'MapillaryDataset_v2',
+    'RVD_2cls', 'RVD_3cls', 'RVD_9cls',
+    'RITE',
 ]
--- mmseg/datasets/rite.py
+++ mmseg/datasets/rite.py
@@ -4,14 +4,14 @@ from .basesegdataset import BaseSegDataset
 
 
 @DATASETS.register_module()
-class RVD_3cls(BaseSegDataset):
+class RITE(BaseSegDataset):
     METAINFO = dict(
         classes=('background', 'Arteries', 'Veins'),
         palette=[[120, 120, 120], [180, 120, 120], [6, 230, 230]])
 
     def __init__(self,
-                 img_suffix='.png',
-                 seg_map_suffix='_3cls.png',
+                 img_suffix='.tif',
+                 seg_map_suffix='.png',
                  reduce_zero_label=False,
                  **kwargs) -> None:
         super().__init__(
