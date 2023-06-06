eb59df0f34ce3b923666a945825a950ecef6d6f3
xiaobaishu0097 <du_heming@hotmail.com>
2023-06-06 16:16:29 +1000

    feat: :sparkles: add chase-db1 related configs

---
+++ configs/segmenter/segmenter_vit-b_mask_8xb1-chase_db1-512x512.py
@@ -0,0 +1,24 @@
+_base_ = [
+    '../_base_/models/segmenter_vit-b16_mask.py',
+    '../_base_/datasets/chase_db1_512x512.py', '../_base_/default_runtime.py',
+    '../_base_/schedules/schedule_160k.py'
+]
+crop_size = (512, 512)
+data_preprocessor = dict(size=crop_size)
+model = dict(
+    data_preprocessor=data_preprocessor,
+    decode_head=dict(
+        num_classes=2,
+    ),)
+optimizer = dict(lr=0.001, weight_decay=0.0)
+optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer)
+train_dataloader = dict(
+    # num_gpus: 8 -> batch_size: 8
+    batch_size=1)
+val_dataloader = dict(batch_size=1)
+
+train_cfg = dict(type='IterBasedTrainLoop', max_iters=10000, val_interval=1000)
+default_hooks = dict(
+    checkpoint=dict(
+        type='CheckpointHook', by_epoch=False, interval=2000,
+        save_best='mIoU'),)
---
+++ configs/unet/unet-s5-d16_deeplabv3_4xb4-chase_db1-256x256.py
@@ -0,0 +1,15 @@
+_base_ = [
+    '../_base_/models/deeplabv3_unet_s5-d16.py', '../_base_/datasets/chase_db1_256x256.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
+]
+crop_size = (256, 256)
+data_preprocessor = dict(size=crop_size)
+model = dict(
+    data_preprocessor=data_preprocessor,
+    test_cfg=dict(crop_size=(256, 256), stride=(170, 170)))
+
+train_cfg = dict(type='IterBasedTrainLoop', max_iters=10000, val_interval=1000)
+default_hooks = dict(
+    checkpoint=dict(
+        type='CheckpointHook', by_epoch=False, interval=2000,
+        save_best='mIoU'),)
