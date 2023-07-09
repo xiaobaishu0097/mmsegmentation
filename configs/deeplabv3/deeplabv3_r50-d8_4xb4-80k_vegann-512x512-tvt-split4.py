958828dad991bac50859026593868d41738de473
xiaobaishu0097 <du_heming@hotmail.com>
2023-07-09 14:44:05 +0000

    update configs

---
+++ configs/deeplabv3/deeplabv3_r50-d8_4xb4-80k_vegann-512x512-tvt-split4.py
@@ -0,0 +1,41 @@
+_base_ = [
+    "../_base_/models/deeplabv3_r50-d8.py",
+    "../_base_/datasets/vegann.py",
+    "../_base_/default_runtime.py",
+    "../_base_/schedules/schedule_80k.py",
+]
+crop_size = (512, 512)
+data_preprocessor = dict(size=crop_size)
+model = dict(
+    data_preprocessor=data_preprocessor,
+    decode_head=dict(num_classes=150),
+    auxiliary_head=dict(num_classes=150),
+)
+
+train_dataloader = dict(
+    batch_size=6,
+    dataset=dict(
+        img_suffix="-TVT-split4.png",
+        seg_map_suffix="-TVT-split4.png",
+    ),
+)
+val_dataloader = dict(
+    dataset=dict(
+        img_suffix="-TVT-split4.png",
+        seg_map_suffix="-TVT-split4.png",
+    )
+)
+test_dataloader = dict(
+    dataset=dict(
+        img_suffix="-TVT-split4.png",
+        seg_map_suffix="-TVT-split4.png",
+    )
+)
+
+train_cfg = dict(type="IterBasedTrainLoop", max_iters=30000, val_interval=5000)
+
+default_hooks = dict(
+    checkpoint=dict(
+        type="CheckpointHook", by_epoch=False, interval=5000, save_best="mIoU"
+    ),
+)
