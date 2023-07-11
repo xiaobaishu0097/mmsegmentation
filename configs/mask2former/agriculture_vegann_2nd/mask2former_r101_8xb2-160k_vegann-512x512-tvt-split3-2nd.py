02a9fc173023bf08cb7edffafca5e14b1ab4fc96
xiaobaishu0097 <du_heming@hotmail.com>
2023-07-11 07:03:25 +0000

    update configs

---
+++ configs/mask2former/agriculture_vegann_2nd/mask2former_r101_8xb2-160k_vegann-512x512-tvt-split3-2nd.py
@@ -0,0 +1,11 @@
+_base_ = ['./mask2former_r50_8xb2-160k_vegann-512x512-tvt-split3.py']
+
+model = dict(
+    backbone=dict(
+        depth=101,
+        init_cfg=dict(type='Pretrained',
+                      checkpoint='torchvision://resnet101')))
+
+train_dataloader = dict(
+    batch_size=10,
+    num_workers=8,)
